5b03ada8990e8a2a5011be75920f4c85787ae5d3
Matt Raible <matt.raible@okta.com>
2018-01-11 23:13:39 -0700

    Initial checkin with EnableResourceServer

---
+++ src/main/java/com/okta/developer/crypto/CryptoApplication.java
@@ -0,0 +1,47 @@
+package com.okta.developer.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
+
+@EnableResourceServer
+@RestController
+@SpringBootApplication
+public class CryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoApplication.class, args);
+	}
+
+    @GetMapping("/hello-oauth")
+    public String sayHello(Principal principal) {
+        return "Hello, " + principal.getName();
+    }
+
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
