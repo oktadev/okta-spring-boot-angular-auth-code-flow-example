dad01b252c1e70bc7cd58db6420a6a30743ae0a8
Brian Demers <bdemers@apache.org>
2018-04-25 11:40:28 -0400

    Renamed HoldingsApiIT classes

--- holdings-api/src/test/java/com/okta/developer/its/HoldingsApiIT.java
+++ holdings-api/src/test/java/com/okta/developer/its/HoldingsApiIT.java
@@ -1,61 +1,60 @@
 package com.okta.developer.its;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.okta.developer.holdingsapi.Holding;
 import com.okta.developer.holdingsapi.HoldingsApiApplication;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.TestPropertySourceUtils;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.SocketUtils;
 import org.springframework.util.StreamUtils;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.client.ResponseErrorHandler;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-
+import static com.spotify.hamcrest.pojo.IsPojo.pojo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.securityContext;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(initializers = HoldingsApiIT.RandomPortInitializer.class)
-@SpringBootTest(classes = {
-                    HoldingsApiIT.TestResourceServerConfiguration.class,
-                    HoldingsApiApplication.class
-                },
+@SpringBootTest(classes = {HoldingsApiApplication.class},
                 webEnvironment = RANDOM_PORT,
                 properties = {
                     "okta.client.token=FAKE_TEST_TOKEN",
+                    "okta.oauth2.localTokenValidation=false",
+                    "okta.oauth2.discoveryDisabled=true",
                     "okta.client.orgUrl=http://localhost:${wiremock.server.port}",
+                    "okta.oauth2.issuer=http://localhost:${wiremock.server.port}/oauth/issuer",
+                    "security.oauth2.resource.userInfoUri=http://localhost:${wiremock.server.port}/oauth/userInfoUri"
                 })
 public class HoldingsApiIT {
 
+    private final static String TEST_ACCESS_TOKEN = "fake-access-token";
+    private final static String TEST_USER_EMAIl = "joe.coder@example.com";
     private final static String TEST_USER_ID = "user-id-123";
 
     private WireMockServer wireMockServer;
@@ -64,39 +63,53 @@ public class HoldingsApiIT {
     private int mockServerPort;
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private TestRestTemplate restTemplate;
 
-    @Test
-    @WithMockUser(username=TEST_USER_ID)
-    public void testGetHoldings() throws Exception {
+    @PostConstruct
+    private void configureRestTemplate() {
 
-        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
+        restTemplate.getRestTemplate().setInterceptors(Collections.singletonList((request, body, execution) -> {
+            request.getHeaders().add("Authorization", "Bearer "+ TEST_ACCESS_TOKEN);
+            return execution.execute(request, body);
+        }));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/holdings")
-                .accept(MediaType.APPLICATION_JSON)
-                .with(securityContext(SecurityContextHolder.getContext())))
-                .andExpect(status().is(200))
+        restTemplate.getRestTemplate().setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                return !response.getStatusCode().is2xxSuccessful();
+            }
 
-                .andExpect(jsonPath("$[0].amount").value("amount-1"))
-                .andExpect(jsonPath("$[0].crypto").value("crypto-1"))
-                .andExpect(jsonPath("$[0].currency").value("currency-1"))
-
-                .andExpect(jsonPath("$[1].amount").value("amount-2"))
-                .andExpect(jsonPath("$[1].crypto").value("crypto-2"))
-                .andExpect(jsonPath("$[1].currency").value("currency-2"));
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                String body = StreamUtils.copyToString(response.getBody(), StandardCharsets.UTF_8);
+                Assert.fail("Http response failed with non-2xx status code: " + response.getStatusCode() +"\n" +
+                        "body:\n" + body);
+            }
+        });
     }
 
     @Test
-    @WithMockUser(username=TEST_USER_ID)
-    public void testPostHoldings() throws Exception {
+    public void testGetHoldings() {
 
-        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
+        List<Holding> holdings = Arrays.asList(restTemplate.getForObject("/api/holdings", Holding[].class));
 
-        Holding[] holdings = {
+        // use Spotify's hamcrest-pojo to validate the objects
+        assertThat(holdings, contains(
+                pojo(Holding.class)
+                    .withProperty("crypto",   is("crypto-1"))
+                    .withProperty("currency", is("currency-1"))
+                    .withProperty("amount",   is("amount-1")),
+                pojo(Holding.class)
+                    .withProperty("crypto",   is("crypto-2"))
+                    .withProperty("currency", is("currency-2"))
+                    .withProperty("amount",   is("amount-2"))
+        ));
+    }
+
+    @Test
+    public void testPostHoldings() {
+
+        Holding[] inputHoldings = {
                 new Holding()
                     .setCrypto("crypto-1")
                     .setCurrency("currency-1")
@@ -107,36 +120,31 @@ public class HoldingsApiIT {
                     .setAmount("amount-2")
         };
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/holdings")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .with(securityContext(SecurityContextHolder.getContext()))
-                .content(new ObjectMapper().writeValueAsString(holdings)))
+        List<Holding> outputHoldings = Arrays.asList(restTemplate.postForObject("/api/holdings", inputHoldings, Holding[].class));
 
-                .andExpect(status().is(200))
+        // output is the same as the input
+        assertThat(outputHoldings, contains(
+                pojo(Holding.class)
+                    .withProperty("crypto",   is("crypto-1"))
+                    .withProperty("currency", is("currency-1"))
+                    .withProperty("amount",   is("amount-1")),
+                pojo(Holding.class)
+                    .withProperty("crypto",   is("crypto-2"))
+                    .withProperty("currency", is("currency-2"))
+                    .withProperty("amount",   is("amount-2"))
+        ));
 
-                .andExpect(jsonPath("$[0].amount").value("amount-1"))
-                .andExpect(jsonPath("$[0].crypto").value("crypto-1"))
-                .andExpect(jsonPath("$[0].currency").value("currency-1"))
-
-                .andExpect(jsonPath("$[1].amount").value("amount-2"))
-                .andExpect(jsonPath("$[1].crypto").value("crypto-2"))
-                .andExpect(jsonPath("$[1].currency").value("currency-2"));
+        // match a few of the encoded json values (the
+        wireMockServer.verify(
+                WireMock.putRequestedFor(WireMock.urlEqualTo("/api/v1/users/" + TEST_USER_ID))
+                     .withRequestBody(WireMock.matchingJsonPath("$.profile.holdings", WireMock.containing("\"amount\":\"amount-1\"")))
+                    .withRequestBody(WireMock.matchingJsonPath("$.profile.holdings", WireMock.containing("\"crypto\":\"crypto-2\""))));
     }
 
     @Before
     public void startMockServer() throws IOException {
         wireMockServer = new WireMockServer(wireMockConfig().port(mockServerPort));
-
-        // load a JSON file from the classpath
-        String body = StreamUtils.copyToString(getClass().getResourceAsStream("/its/user.json"), StandardCharsets.UTF_8);
-
-        wireMockServer.stubFor(WireMock.get("/api/v1/users/" + TEST_USER_ID)
-                .willReturn(aResponse().withBody(body)));
-
-        wireMockServer.stubFor(WireMock.put("/api/v1/users/" + TEST_USER_ID)
-                .willReturn(aResponse().withBody(body)));
-
+        configureWireMock();
         wireMockServer.start();
     }
 
@@ -147,6 +155,30 @@ public class HoldingsApiIT {
         }
     }
 
+    private void configureWireMock() throws IOException {
+         // load a JSON file from the classpath
+        String body = StreamUtils.copyToString(getClass().getResourceAsStream("/its/user.json"), StandardCharsets.UTF_8);
+
+        // respond to GET for user
+        wireMockServer.stubFor(WireMock.get("/api/v1/users/" + TEST_USER_EMAIl)
+                .willReturn(aResponse().withBody(body)));
+
+        // respond to PUT for user
+        wireMockServer.stubFor(WireMock.put("/api/v1/users/" + TEST_USER_ID)
+                .willReturn(aResponse().withBody(body)));
+
+        // OAuth userInfoUri
+        String userInfoBody = StreamUtils.copyToString(getClass().getResourceAsStream("/its/userInfo.json"), StandardCharsets.UTF_8);
+        wireMockServer.stubFor(
+                WireMock.get("/oauth/userInfoUri")
+                    .withHeader("Authorization", WireMock.equalTo("Bearer "+ TEST_ACCESS_TOKEN))
+                .willReturn(aResponse()
+                        .withBody(userInfoBody)
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                ));
+
+    }
+
     public static class RandomPortInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
         @Override
@@ -155,14 +187,6 @@ public class HoldingsApiIT {
             TestPropertySourceUtils.addInlinedPropertiesToEnvironment(applicationContext,
                     "wiremock.server.port=" + randomPort
             );
-        }
-    }
-
-    public static class TestResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-
-        @Override
-        public void configure(ResourceServerSecurityConfigurer security) {
-            security.stateless(false);
         }
     }
 }
