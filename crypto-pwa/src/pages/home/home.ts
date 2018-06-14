7e52b27c54e20ef5fd19815e626d730e68f8dfd4
Matt Raible <matt.raible@okta.com>
2018-06-14 12:54:42 -0600

    Remove console.log

--- crypto-pwa/src/pages/home/home.ts
+++ crypto-pwa/src/pages/home/home.ts
@@ -40,7 +40,6 @@ export class HomePage {
 
   logout() {
     this.userProvider.logout().subscribe((response: any) => {
-      console.log(response);
       if (response.logoutUrl) {
         location.href = response.logoutUrl + "?id_token_hint=" + response.idToken + "&post_logout_redirect_uri=" + window.location.origin;
       } else {
